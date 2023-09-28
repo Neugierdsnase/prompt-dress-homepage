da0a54f07804e8346834288cebfac8ee55aeb47a
Konstantin Kovar <mail@vomkonstant.in>
2023-09-28 19:23:06 +0200

    reverted some stuff because github build action failed

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -195,7 +195,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "10q33ao"
+  version_hash: "qve0v1"
 };
 function get_hooks() {
   return {};
